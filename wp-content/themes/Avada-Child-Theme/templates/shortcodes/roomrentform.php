96941f0b3d7a893c0f0f7da18c4ea7187a99b8fb
mistvan22 <pocokxtrame@gmail.com>
2016-12-14 14:42:20 +0100

    Save

--- wp-content/themes/Avada-Child-Theme/templates/shortcodes/roomrentform.php
+++ wp-content/themes/Avada-Child-Theme/templates/shortcodes/roomrentform.php
@@ -1,16 +1,16 @@
-<h1><?php echo __('Ajánlatkérés', 'hotel'); ?></h1>
-<h2><?php echo __('Adja meg az alábbi űrlapon az ajánlatkéréshez szükséges adatokat.', 'hotel'); ?></h2>
+<h1><?php echo __('Ajánlatkérés', 'Avada'); ?></h1>
+<h2><?php echo __('Adja meg az alábbi űrlapon az ajánlatkéréshez szükséges adatokat.', 'Avada'); ?></h2>
 
 <form class="" id="mailsend" action="index.html" method="post">
   <div class="room-rent-wrapper date-container">
     <div class="input-holder">
-      <div class="input input-label"><h3><div class="stn">1</div><?php echo __('Mennyi időre szeretnének szobát foglalni?', 'hotel'); ?></h3></div>
+      <div class="input input-label"><h3><div class="stn">1</div><?php echo __('Mennyi időre szeretnének szobát foglalni?', 'Avada'); ?></h3></div>
       <div class="input date_from">
-        <label for="date_from"><?php echo __('Érkezés ideje', 'hotel'); ?> *</label>
+        <label for="date_from"><?php echo __('Érkezés ideje', 'Avada'); ?> *</label>
         <input type="text" class="datepicker" name="roomrent[date][from]" readonly="readonly" id="date_from" value="<?=date('Y / m / d', strtotime('+ 1 days'))?>">
       </div>
       <div class="input date_to">
-        <label for="date_to"><?php echo __('Távozás ideje', 'hotel'); ?> *</label>
+        <label for="date_to"><?php echo __('Távozás ideje', 'Avada'); ?> *</label>
         <input type="text" class="datepicker" name="roomrent[date][to]" readonly="readonly" id="date_to" value="<?=date('Y / m / d', strtotime('+ 3 days'))?>">
       </div>
     </div>
@@ -18,41 +18,41 @@
 
   <div class="room-rent-wrapper">
       <div class="input-holder">
-        <div class="input input-label"><h3><div class="stn">2</div><?php echo __('Ajánlatkérés paraméterek megadása', 'hotel'); ?></h3></div>
+        <div class="input input-label"><h3><div class="stn">2</div><?php echo __('Ajánlatkérés paraméterek megadása', 'Avada'); ?></h3></div>
         <div class="input people_adults">
-          <label for="people_adults"><?php echo __('Felnőttek', 'hotel'); ?> *</label>
+          <label for="people_adults"><?php echo __('Felnőttek', 'Avada'); ?> *</label>
           <input type="number" name="roomrent[people][adults]" id="people_adults" value="2" min="1" step="1">
         </div>
         <div class="input people_children">
-          <label for="people_children"><?php echo __('Gyerekek', 'hotel'); ?></label>
+          <label for="people_children"><?php echo __('Gyerekek', 'Avada'); ?></label>
           <input type="number" name="roomrent[people][children]" id="people_children" value="0" min="0" step="1">
         </div>
         <div class="input settings_children_eroom">
-          <label for="settings_children_eroom"><?php echo __('Gyerekek külön szobában', 'hotel'); ?></label>
+          <label for="settings_children_eroom"><?php echo __('Gyerekek külön szobában', 'Avada'); ?></label>
           <input type="checkbox" name="roomrent[settings][children_eroom]" id="settings_children_eroom"><label for="settings_children_eroom"></label>
         </div>
         <div class="input settings_visittype">
-          <label for="settings_visittype"><?php echo __('Foglalás jellege', 'hotel'); ?> *</label>
+          <label for="settings_visittype"><?php echo __('Foglalás jellege', 'Avada'); ?> *</label>
           <select class="" name="roomrent[settings][visittype]" id="settings_visittype">
-            <option value="">-- <?php echo __('válasszon', 'hotel'); ?> --</option>
+            <option value="">-- <?php echo __('válasszon', 'Avada'); ?> --</option>
             <option value="" disabled="disabled"></option>
-            <option value="Szabadidő"><?php echo __('Szabadidő', 'hotel'); ?></option>
-            <option value="Üzleti"><?php echo __('Üzleti', 'hotel'); ?></option>
+            <option value="Szabadidő"><?php echo __('Szabadidő', 'Avada'); ?></option>
+            <option value="Üzleti"><?php echo __('Üzleti', 'Avada'); ?></option>
           </select>
         </div>
         <div class="input settings_roomtype">
-          <label for="settings_roomtype"><?php echo __('Milyen szobát szeretne?', 'hotel'); ?> *</label>
+          <label for="settings_roomtype"><?php echo __('Milyen szobát szeretne?', 'Avada'); ?> *</label>
           <select class="" name="roomrent[settings][roomtype]" id="settings_roomtype">
-            <option value="">-- <?php echo __('válasszon', 'hotel'); ?> --</option>
+            <option value="">-- <?php echo __('válasszon', 'Avada'); ?> --</option>
             <option value="" disabled="disabled"></option>
-            <option value="Strandard Szoba"><?php echo __('Strandard szoba (2 személyes)', 'hotel'); ?></option>
-            <option value="Komfort Szoba"><?php echo __('Komfort szoba (2 - 4 személyes)', 'hotel'); ?></option>
-            <option value="Superior Családi Szoba"><?php echo __('Superior családi szoba (2 - 4 személyes)', 'hotel'); ?></option>
+            <option value="Strandard Szoba"><?php echo __('Strandard szoba (2 személyes)', 'Avada'); ?></option>
+            <option value="Komfort Szoba"><?php echo __('Komfort szoba (2 - 4 személyes)', 'Avada'); ?></option>
+            <option value="Superior Családi Szoba"><?php echo __('Superior családi szoba (2 - 4 személyes)', 'Avada'); ?></option>
           </select>
         </div>
         <div class="input setting_children_ages">
-          <label for="settings_children_ages"><?php echo __('Gyerek(ek) kora', 'hotel'); ?></label>
-          <input type="text" name="roomrent[settings][children_ages]" id="settings_children_ages" placeholder="<?php echo __('Írja be a gyerek(ek) korát.', 'hotel'); ?>">
+          <label for="settings_children_ages"><?php echo __('Gyerek(ek) kora', 'Avada'); ?></label>
+          <input type="text" name="roomrent[settings][children_ages]" id="settings_children_ages" placeholder="<?php echo __('Írja be a gyerek(ek) korát.', 'Avada'); ?>">
           <small style="color: #b3b3b3;"><?=__('Több gyerek esetén vesszővel válassza el.','hotel')?></small>
         </div>
       </div>
@@ -60,29 +60,29 @@
 
   <div class="room-rent-wrapper">
     <div class="input-holder">
-      <div class="input input-label"><h3><div class="stn">3</div><?php echo __('Kapcsolattartó adatok', 'hotel'); ?></h3></div>
+      <div class="input input-label"><h3><div class="stn">3</div><?php echo __('Kapcsolattartó adatok', 'Avada'); ?></h3></div>
       <div class="input contact_vezeteknev">
-        <label for="contact_vezeteknev"><?php echo __('Vezetéknév', 'hotel'); ?> *</label>
+        <label for="contact_vezeteknev"><?php echo __('Vezetéknév', 'Avada'); ?> *</label>
         <input type="text" name="roomrent[contact][vezeteknev]" id="contact_vezeteknev" value="">
       </div>
       <div class="input contact_keresztnev">
-        <label for="contact_keresztnev"><?php echo __('Keresztnév', 'hotel'); ?> *</label>
+        <label for="contact_keresztnev"><?php echo __('Keresztnév', 'Avada'); ?> *</label>
         <input type="text" name="roomrent[contact][keresztnev]" id="contact_keresztnev" value="">
       </div>
       <div class="input contact_telefon">
-        <label for="contact_telefon"><?php echo __('Telefonszám', 'hotel'); ?> *</label>
+        <label for="contact_telefon"><?php echo __('Telefonszám', 'Avada'); ?> *</label>
         <input type="text" name="roomrent[contact][telefon]" id="contact_telefon" placeholder="+36 (XX) XXX-XXXX" value="">
       </div>
       <div class="input contact_email">
-        <label for="contact_email"><?php echo __('E-mail cím', 'hotel'); ?> *</label>
+        <label for="contact_email"><?php echo __('E-mail cím', 'Avada'); ?> *</label>
         <input type="text" name="roomrent[contact][email]" id="contact_email" value="">
       </div>
       <div class="input contact_msg">
-        <label for="contact_msg"><?php echo __('Megjegyzés', 'hotel'); ?></label>
+        <label for="contact_msg"><?php echo __('Megjegyzés', 'Avada'); ?></label>
         <textarea name="roomrent[contact][msg]" id="contact_msg"></textarea>
       </div>
       <div class="input send-mail">
-        <button type="button" id="mail-sending-btn" class="fusion-button" onclick="ajanlatkeresKuldes();" name="button"><?php echo __('Ajánlatkérés küldése', 'hotel'); ?> <i class="fa fa-envelope-o"></i></button>
+        <button type="button" id="mail-sending-btn" class="fusion-button" onclick="ajanlatkeresKuldes();" name="button"><?php echo __('Ajánlatkérés küldése', 'Avada'); ?> <i class="fa fa-envelope-o"></i></button>
       </div>
     </div>
   </div>
@@ -144,7 +144,7 @@ var mail_sended = 0;
 function ajanlatkeresKuldes()
 {
   if(mail_sending_progress == 0 && mail_sended == 0){
-    jQuery('#mail-sending-btn').html('<?php echo __('Küldés folyamatban', 'hotel'); ?> <i class="fa fa-spinner fa-spin"></i>').addClass('in-progress');
+    jQuery('#mail-sending-btn').html('<?php echo __('Küldés folyamatban', 'Avada'); ?> <i class="fa fa-spinner fa-spin"></i>').addClass('in-progress');
     jQuery('#mailsend .missing').removeClass('missing');
 
     mail_sending_progress = 1;
@@ -156,9 +156,9 @@ function ajanlatkeresKuldes()
         var resp = jQuery.parseJSON(data);
         if(resp.error == 0) {
           mail_sended = 1;
-          jQuery('#mail-sending-btn').html('<?php echo __('Ajánlatkérés elküldve', 'hotel'); ?> <i class="fa fa-check-circle"></i>').removeClass('in-progress').addClass('sended');
+          jQuery('#mail-sending-btn').html('<?php echo __('Ajánlatkérés elküldve', 'Avada'); ?> <i class="fa fa-check-circle"></i>').removeClass('in-progress').addClass('sended');
         } else {
-          jQuery('#mail-sending-btn').html('<?php echo __('Ajánlatkérés küldése', 'hotel'); ?> <i class="fa fa-envelope-o"></i>').removeClass('in-progress')
+          jQuery('#mail-sending-btn').html('<?php echo __('Ajánlatkérés küldése', 'Avada'); ?> <i class="fa fa-envelope-o"></i>').removeClass('in-progress')
           mail_sending_progress = 0;
           if(resp.missing != 0) {
             jQuery.each(resp.missing_elements, function(i,e){
